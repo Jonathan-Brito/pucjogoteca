1c9b40b89980718a1103463ad74c210ef085425a
Jonathan Brito <devjonathanbrito@gmail.com>
2023-07-12 20:35:36 -0300

    Revisando e fazendo ajustes em pontos do sistema e implementando todas as rotas e integração com o banco de dados e separando os arquivos

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
-SECRET_KEY = 'aluno'
+import os
+
+SECRET_KEY = 'alura'
 
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-
         SGBD = 'mysql+mysqlconnector',
         usuario = 'root',
-        senha = '420024Jojo.,',
+        senha = 'admin',
         servidor = 'localhost',
-        database = 'pucjogoteca'
+        database = 'jogoteca'
     )
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
--- models.py
+++ models.py
@@ -7,7 +7,8 @@ class Jogos(db.Model):
     console = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return '<Name %r>' % self.nome
+        return '<Name %r>' % self.name
+
 
 class Usuarios(db.Model):
     nickname = db.Column(db.String(8), primary_key=True)
@@ -15,4 +16,4 @@ class Usuarios(db.Model):
     senha = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return '<Name %r>' % self.nome
+        return '<Name %r>' % self.name
